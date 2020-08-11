b92701d9c880ce5809aa32d81988ec58b31978b8
sagar2702 <sagarchikhale270298@gmail.com>
2020-08-11 17:52:12 +0530

    buySell problem

---
+++ buy_and_sell.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+ {
+     int n;
+     cin>>n;
+     int arr[n];
+     for(int i=0;i<n;i++){
+         cin>>arr[i];
+     }
+    int max1=0,max2=0,flag=0;
+    for(int i=0;i<n;i++){
+        flag=0;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[j-1]){
+                flag=1;
+                int x=arr[j-1]-arr[i];
+                if(x>max1){
+                   max2=max1;
+                   max1=x;
+                } 
+                i=j-1;
+                break;
+            }
+        }
+        if(flag==0){
+            int x=arr[n-1]-arr[i];
+            if(x>max1){
+                   max2=max1;
+                   max1=x;
+                } 
+            break;
+        }
+    }
+    cout<<max1+max2<<endl;
+	return 0;
+ }
